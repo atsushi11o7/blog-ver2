30299b0326034e1c50c676a125ac1dcbd8093f33
atsushi11o7 <130956694+atsushi11o7@users.noreply.github.com>
2024-07-26 01:49:59 +0900

    fix logo

--- fumiblog/src/components/atoms/Logo/index.tsx
+++ fumiblog/src/components/atoms/Logo/index.tsx
@@ -3,28 +3,28 @@ import Image from "next/image";
 import styled from "styled-components";
 
 const ImageLink = styled.a`
-  display: inline-block;
-  font-size: var(--small-heading2);
-  font-weight: 700;
-  letter-spacing: 0.15em;
+    display: inline-block;
+    font-size: var(--small-heading2);
+    font-weight: 700;
+    letter-spacing: 0.15em;
 `;
 
 const Logo: React.FC = () => {
-  return (
-    <Link href="/">
-      <ImageLink>
-        <figure>
-          <Image
-            src="/logo.png"
-            width={200}
-            height={120}
-            alt="logo"
-            sizes="(min-width: 768px) 200px, 100vw"
-          />
-        </figure>
-      </ImageLink>
-    </Link>
-  );
+    return (
+        <Link href="/">
+            <ImageLink>
+                <figure>
+                    <Image
+                        src="/logo.png"
+                        width={200}
+                        height={120}
+                        alt="logo"
+                        sizes="(min-width: 768px) 200px, 100vw"
+                    />
+                </figure>
+            </ImageLink>
+        </Link>
+    );
 };
 
 export default Logo;
