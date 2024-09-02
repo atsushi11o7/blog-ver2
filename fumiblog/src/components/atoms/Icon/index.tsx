0c42866f80674372d98a870aac2ddd48b600a7f1
atsushi11o7 <130956694+atsushi11o7@users.noreply.github.com>
2024-09-03 00:30:32 +0900

    add icon

---
+++ fumiblog/src/components/atoms/Icon/index.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import styled from 'styled-components';
+
+type IconProps = {
+  size?: number;
+};
+
+const StyledIcon = styled.div<{ size: number }>`
+  width: ${({ size }) => size}px;
+  height: ${({ size }) => size}px;
+  border-radius: 50%;
+  overflow: hidden;
+  display: inline-block;
+`;
+
+const Icon: React.FC<IconProps>= ({
+    size = 120
+}) => (
+  <StyledIcon size={size}>
+    <Image 
+        src="/icon.png"
+        width={size}
+        height={size}
+        alt="icon"
+    />
+  </StyledIcon>
+);
+
+export default Icon;
---
+++ fumiblog/src/components/organisms/Header/index.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Header from './index'
+
+export default {
+    title: 'Organisms/Header',
+    component: Header,
+} as Meta;
+  
+
+export const Default: StoryObj<typeof Header> = {};
